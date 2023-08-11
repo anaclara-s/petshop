78a552e0c8b8abc104b7404b1d51e388630e2ab4
Ana <anaclarasantos.amorin.com>
2023-08-11 20:59:53 -0300

    created registration screen

--- lib/registrationscreen.dart
+++ lib/registrationscreen.dart
@@ -6,21 +6,169 @@ class Registration extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: Color.fromARGB(255, 61, 54, 73),
+      //
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'CRIAR CONTA',
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 30,
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextFormField(
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.amber,
+                  enabledBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 60, 255, 0),
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Colors.limeAccent,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.person_outline,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  labelText: 'NOME',
+                  labelStyle: TextStyle(
+                    color: Colors.brown,
+                  ),
+                ),
               ),
-            ),
-          ],
+              //
+              SizedBox(
+                width: 300,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextFormField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 35, 16, 105),
+                        enabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.orange,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.email_outlined,
+                          color: Colors.pinkAccent,
+                        ),
+                        labelText: 'EMAIL',
+                        labelStyle: TextStyle(
+                          color: Colors.amberAccent,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //
+              SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextFormField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.brown,
+                        enabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.green,
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.phone_android,
+                          color: Colors.amberAccent,
+                        ),
+                        labelText: 'CELULAR',
+                        labelStyle: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //
+              SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextFormField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.brown,
+                        enabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.green,
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Colors.amberAccent,
+                        ),
+                        labelText: 'SENHA',
+                        labelStyle: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //
+              SizedBox(
+                child: ElevatedButton(
+                  child: Text('CRIAR'),
+                  onPressed: () {
+                    print('CRIAR');
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+      //
     );
   }
 }
