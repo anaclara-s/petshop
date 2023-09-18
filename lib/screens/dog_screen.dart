7f0131c77e732573eaf39c61e5dd24e216239e6f
Ana <anaclarasantos.amorin@gmail.com>
2023-09-17 23:59:11 -0300

    adcionado a tela Dog

--- lib/screens/dog_screen.dart
+++ lib/screens/dog_screen.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:petshop/shared/constants/constants.dart';
 
 import '../shared/lists/products/filter_products_list.dart';
 import '../shared/lists/products/products_list.dart';
+import '../shared/themes/bottom_menu.dart';
 
 class DogScreen extends StatelessWidget {
   const DogScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Product> dogItems =
-        selectedItems.where((item) => item.title.contains('Dog')).toList();
+    List<Product> filteredProductsList = filteredDogProducts();
 
     return Scaffold(
       body: Column(
         children: [
-          for (var item in dogItems) Text(item.title),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return buildProductCard(filteredProductsList[index]);
+                },
+                itemCount: filteredProductsList.length,
+              ),
+            ),
+          ),
+          const BottomMenu(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProductCard(Product product) {
+    return Card(
+      child: Column(
+        children: [
+          Image.asset(product.image),
+          Text(product.title),
+          Text('Preço: ${formatNumber.format(product.price)}'),
         ],
       ),
     );
--- lib/shared/lists/products/filter_products_list.dart
+++ lib/shared/lists/products/filter_products_list.dart
@@ -1,9 +1,7 @@
 import 'products_list.dart';
 
-List<Product> selectedItems = products.where((item) {
-  return (item.title.contains('Dog') && item.title.contains('Origens')) ||
-      item.title.contains('Cat') ||
-      (item.title.contains('Calopsita') ||
-          item.title.contains('Aves') ||
-          item.title.contains('Periquito'));
-}).toList();
+List<Product> filteredDogProducts() {
+  return products.where((item) {
+    return item.title.contains('Dog');
+  }).toList();
+}
